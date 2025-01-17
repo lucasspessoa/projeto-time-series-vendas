59c6893209e66af3d1dfb2339ed1864d837f31df
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2025-01-17 09:56:59 -0300

    etapa-17.01

--- time-series-vendas.py
+++ time-series-vendas.py
@@ -21,4 +21,27 @@ decomposicao.seasonal.plot(ax=axes[1], title='Componente Sazonal')
 # Componente residual
 decomposicao.resid.plot(ax=axes[2], title='Componente Residual')
 plt.tight_layout()
-plt.show()
+# plt.show()
+
+# Decompondo o Ruído da Série
+df['Ruído'] = decomposicao.resid
+# Visualizando Série e Ruído graficamente
+df[['ProductP3', 'Ruído']].plot(title='Série e Vendas')
+# plt.show()
+
+# Verificação de estacionariedade
+from statsmodels.tsa.stattools import adfuller
+resultado = adfuller(df['Ruído'].dropna())
+print(f"Estatística do Teste ADF: {resultado[0]}")
+print(f"P-valor: {resultado[1]}")
+# Teste de Hipótese a 5% de Significância
+if resultado[1] <= 0.05:
+    print("A série é estacionária")
+else:
+    print("A série é não estacionária")
+
+# Remoção da Sazonalidade: uso de Diferenciação Sazonal
+df['Diferenciada_Sazonal'] = df['Ruído'] - df['Ruído'].shift(12)
+resultado_sazonal = adfuller(df['Diferenciada_Sazonal'].dropna())
+print(f"P-valor após diferenciação sazonal: {resultado_sazonal[1]}")
+
